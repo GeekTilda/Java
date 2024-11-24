06f12c25c8b0ace1522793b0d4d9c71e8cbab98e
GeekTilda <40564225+GeekTilda@users.noreply.github.com>
2024-11-24 17:46:01 +0100

    added comments

--- MinstaOmkrets.java
+++ MinstaOmkrets.java
@@ -1,30 +1,41 @@
 import java.util.Scanner;
 
+// Calculates the minimum circumference of a given area. 
+
 public class MinstaOmkrets {
     public static void main(String[] args){
         long area = 1;
         Scanner reader = new Scanner(System.in);
         Boolean tf = true;
+
         while (tf) {
             System.out.print("");
             try {
                 area = Long.parseLong(reader.next());
-                tf = false;
+
+                if (area <= 0) {    // If the user puts in a negative value (or zero)
+                    throw new Exception();
+                }
+
+                tf = false; // Break loop
             } catch (Exception e) {
-                System.out.println("Måste vara ett heltal mellan 1 och 10^12.");
+                System.out.println("Måste vara ett positivt heltal mellan 1 och 10^12.");
             }
         }
         reader.close();
+
+        // Initiating values
         long x = 1;
-        long s = 1;
+        long s = 1; // circumference
         long smin = Long.MAX_VALUE;
         double sqrt = Math.sqrt(area);
+
         while (x <= sqrt) {
             if (area % x == 0) {
-                long y = area/x;
-                s = 2 * (y + x);
+                long y = area/x;    // area = x*y
+                s = 2 * (y + x);    // circumference = 2*(x+y)
                 if (s < smin) {
-                    smin = s;
+                    smin = s;   // Update minimum
                 }
             }
             x++;
